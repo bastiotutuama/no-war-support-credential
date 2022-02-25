7b303f42c457652bed27e34479958bb8fcf746aa
Sebastian Otutuama <mail@basti.io>
2022-02-25 18:45:49 +0100

    added ssg to page data passing skeleton

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,33 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
-export default function Home() {
+export async function getServerSideProps(context) {
+  console.log("ssr log test");
+
+  const supporterList = [
+    {
+      name: "first",
+      id: "1",
+      logo_url:"https://example.com/one.png"
+    },
+    {
+      name: "second",
+      id: "2",
+      logo_url:"https://example.com/second.png"
+    }
+  ]
+
+  return {
+    props: {
+      supporterList
+    }, // will be passed to the page component as props
+  }
+}
+
+export default function Home(props) {
+
+  console.log(props.supporterList)
+
   return (
     <div className="container">
       <Head>
